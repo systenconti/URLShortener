ca6067852f29d8469c44a30f60a6193654eea33f
Bartosz Sowa <bartoszsowa.bsi@gmail.com>
2023-05-02 17:14:01 +0200

    Create PostgreSQL connection DB Model

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from credentials import secret_key
+import os
+
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = secret_key
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = f"postgresql://postgres:{os.environ.get('DATABASE_PASS')}@localhost:5432/urlshortener"
+
+db = SQLAlchemy(app)
+
+
+class Urls(db.Model):
+    url_id = db.Column("url_id", db.Integer, primary_key=True)
+    long = db.Column("long", db.String())
+    short = db.Column("short", db.String(4))
+
+    def __init__(self, long, short):
+        self.long = long
+        self.short = short
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -14,4 +34,6 @@ def shortener():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)

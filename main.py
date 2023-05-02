7a675ba4ad549935c1395b87e112d2c2e2633891
Bartosz Sowa <bartoszsowa.bsi@gmail.com>
2023-05-02 14:16:18 +0200

    Get long URL with request method

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def shortener():
-    return render_template("index.html")
+    if request.method == "POST":
+        url_received = request.form["long_url"]
+        print(url_received)
+        return url_received
+    else:
+        return render_template("index.html")
 
 
 if __name__ == "__main__":
